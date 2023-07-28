0e7a73473ade8e08aa54b9ebff43ce109b6c392f
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-07-29 00:50:54 +0300

    feat: add validation and status codes for users

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { DatabaseService } from 'src/database/database.service';
 import { IUser } from 'src/models/interfaces';
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4, validate as uuidValidate } from 'uuid';
 
 @Injectable()
 export class UserService {
@@ -30,31 +35,66 @@ export class UserService {
   }
 
   findOne(id: string) {
+    const isIdValid = uuidValidate(id);
+
+    if (!isIdValid) {
+      throw new BadRequestException(
+        `Sorry, userId ${id} is invalid (not uuid)`,
+      );
+    }
+
     const foundUser = this.db.users.find((user) => user.id === id);
-    return this.hidePasswordUser(foundUser);
+    if (foundUser) {
+      return this.hidePasswordUser(foundUser);
+    } else {
+      throw new NotFoundException(`Sorry, user with id ${id} not found`);
+    }
   }
 
   updatePassword(id: string, updateUserDto: UpdateUserDto) {
+    const isIdValid = uuidValidate(id);
+
+    if (!isIdValid) {
+      throw new BadRequestException(
+        `Sorry, userId ${id} is invalid (not uuid)`,
+      );
+    }
+
     const userToUpdate = this.db.users.find((user) => user.id === id);
+    if (!userToUpdate) {
+      throw new NotFoundException(`Sorry, user with id ${id} not found`);
+    }
+
     const { oldPassword, newPassword } = updateUserDto;
 
     if (oldPassword === userToUpdate.password) {
-      //update is possible
       userToUpdate.password = newPassword;
       userToUpdate.version += 1;
       userToUpdate.updatedAt = Date.now();
     } else {
-      console.log('Old password is incorrect, status code 403');
+      throw new ForbiddenException(`Old password is incorrect`);
     }
 
     return this.hidePasswordUser(userToUpdate);
   }
 
   remove(id: string) {
+    const isIdValid = uuidValidate(id);
+
+    if (!isIdValid) {
+      throw new BadRequestException(
+        `Sorry, userId ${id} is invalid (not uuid)`,
+      );
+    }
+
     const userToDelete = this.db.users.find((user) => user.id === id);
-    const userIndexToDelete = this.db.users.indexOf(userToDelete);
-    this.db.users.splice(userIndexToDelete, 1);
-    return `This action removes a #${id} user`;
+    if (userToDelete) {
+      const userIndexToDelete = this.db.users.indexOf(userToDelete);
+      this.db.users.splice(userIndexToDelete, 1);
+      return `User with id #${id} was removed`;
+    } else {
+      throw new NotFoundException(`Sorry, user with id ${id} not found`);
+    }
   }
 
   hidePasswordUser(user: IUser) {
