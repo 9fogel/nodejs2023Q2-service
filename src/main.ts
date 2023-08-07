d8489341362a654fe802fd93f72f2999c301aa2f
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-08-07 16:48:01 +0300

    fix: fix .env and docker-compose

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('port') || 4000;
+  console.log(port);
 
   const filePath = path.resolve(__dirname, 'doc', 'api.yaml');
   const document: any = yaml.load(fs.readFileSync(filePath, 'utf8'));
