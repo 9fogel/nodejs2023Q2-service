27cfe1257ef824c5c3febba9d66dcffe50966cc4
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-08-20 02:28:14 +0300

    feat: add authGuard and skipAuth decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,19 @@
 import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { SkipAuth } from './auth.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @SkipAuth()
   @Post('signup')
   signup(@Body() loginDto: LoginDto) {
     return this.authService.signup(loginDto);
   }
 
+  @SkipAuth()
   @HttpCode(HttpStatus.OK)
   @Post('login')
   login(@Body() loginDto: LoginDto) {
